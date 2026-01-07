d538fa75c2ef96c9107580c528b6f94c0b72e006
AmericanSurvivalst <americansurvivalist.us@gmail.com>
2026-01-07 00:27:19 -0500

    Delete wrentham-gazette directory

--- wrentham-gazette/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as ArticleCard } from './ArticleCard';
-export { default as NewsletterSignup } from './NewsletterSignup';
-export { default as PoliceLogSection } from './PoliceLogSection';
-export { default as EventsSection } from './EventsSection';
-export { default as BreakingNews } from './BreakingNews';
-export { default as AdPlaceholder } from './AdPlaceholder';
